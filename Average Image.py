54e65012b80cd3c47d55e96a0e0d53e90ac1d7b6
DannyStruijk <64257942+DannyStruijk@users.noreply.github.com>
2024-03-10 16:31:46 +0100

    Add files via upload

---
+++ Average Image.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 14:02:43 2024
+
+@author: 20192832
+"""
+
+from __future__ import print_function, absolute_import
+import elastix
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio.v2 as imageio
+import os
+import SimpleITK as sitk
+
+#Read image masks from the patients, mhd files    
+atlas_1 = sitk.ReadImage("TrainingData-GroupAssignment\TrainingData\p102\mr_bffe.mhd")    
+atlas_2 = sitk.ReadImage("TrainingData-GroupAssignment\TrainingData\p108\mr_bffe.mhd")        
+atlas_3 = sitk.ReadImage("TrainingData-GroupAssignment\TrainingData\p116\mr_bffe.mhd")
+atlas_4 = sitk.ReadImage("TrainingData-GroupAssignment\TrainingData\p128\mr_bffe.mhd")
+atlas_5 = sitk.ReadImage("TrainingData-GroupAssignment\TrainingData\p129\mr_bffe.mhd")
+
+#Convert image format to a 3 dimensional array to compare the images
+image_1 = sitk.GetArrayViewFromImage(atlas_1)
+image_2 = sitk.GetArrayViewFromImage(atlas_2)
+image_3 = sitk.GetArrayViewFromImage(atlas_3)
+image_4 = sitk.GetArrayViewFromImage(atlas_4)
+image_5 = sitk.GetArrayViewFromImage(atlas_5)
+
+#Obtaining the 3 dimensional shape of the images
+image_shape = image_1.shape
+
+#Creating an empty array which intializes our new atlas image
+combined_atlas = np.zeros((image_shape[0], image_shape[1], image_shape[2]), dtype=int)
+value_list = np.zeros(5, dtype = int)
+
+#Looking at every pixel based on the pixel values for each images, assign new value.
+#New value of the atlas will be 1 if three or more images have pixel value of 1.
+for i in range(0, image_shape[0]):
+    for j in range(0, image_shape[1]):
+        for k in range(0, image_shape[2]):
+            value_list[0] = image_1[i,j,k]
+            value_list[1] = image_2[i,j,k]
+            value_list[2] = image_3[i,j,k]
+            value_list[3] = image_4[i,j,k]
+            value_list[4] = image_5[i,j,k]
+            
+            combined_atlas[i,j,k] = (sum(value_list))/len(value_list)
+            
+            value_list = np.zeros(5, dtype = int)
+
+# Display the image slice from the middle of the stack, z axis
+z = int(atlas_1.GetDepth()/2)
+npa_zslice = sitk.GetArrayViewFromImage(atlas_1)[z,:,:]
+npa_zslice_2 = sitk.GetArrayViewFromImage(atlas_2)[z,:,:]
+npa_zslice_3 = sitk.GetArrayViewFromImage(atlas_3)[z,:,:]
+npa_zslice_4 = sitk.GetArrayViewFromImage(atlas_4)[z,:,:]
+npa_zslice_5 = sitk.GetArrayViewFromImage(atlas_5)[z,:,:]
+
+npa_zslice_atlas = combined_atlas[z, :, :]
+
+# Six plots displaying the binary mask displaying the prostate. The sixth image
+# displays the atlas created by majority voting of 5 prostate images.
+fig = plt.figure(figsize=(10,3))
+
+fig.add_subplot(2,3,1)
+plt.imshow(npa_zslice)
+plt.title('Segmentation of P102', fontsize=10)
+plt.axis('off')
+
+fig.add_subplot(2,3,2)
+plt.imshow(npa_zslice_2)    
+plt.title('Segmentation of P108', fontsize=10)
+plt.axis('off')
+
+fig.add_subplot(2,3,3)
+plt.imshow(npa_zslice_3)
+plt.title('Segmentation of p116', fontsize=10)
+plt.axis('off')
+
+fig.add_subplot(2,3,4)
+plt.imshow(npa_zslice_4)
+plt.title('Segmentation of p128', fontsize=10)
+plt.axis('off')
+
+fig.add_subplot(2,3,5)
+plt.imshow(npa_zslice_5)
+plt.title('Segmentation of p129', fontsize=10)
+plt.axis('off')
+
+fig.add_subplot(2,3,6)
+plt.imshow(npa_zslice_atlas)
+plt.title('Combined Segmentation by Averaging', fontsize=10)
+plt.axis('off')
+
+#Writing the obtained atlas into an MHD file
+atlas_mhd = sitk.GetImageFromArray(combined_atlas)
+sitk.WriteImage(atlas_mhd, "average_value_atlas_v2.mhd")
